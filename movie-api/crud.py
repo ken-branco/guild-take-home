2ee2c6a1a7b008ed4fd31707426cf4831168efa0
Ken Branco <branco.kenneth@gmail.com>
2022-06-27 09:49:28 -0400

    initial crud op to get most popular genre by year

--- movie-api/crud.py
+++ movie-api/crud.py
@@ -1,14 +1,14 @@
-from .models import Movie, ProductionCompany, ProductionMovieLookup
+from .models import (Movie,
+                     Rating,
+                     Genre,
+                     MovieGenreLookup,
+                     ProductionCompany,
+                     ProductionMovieLookup
+                     )
+from sqlalchemy import desc
+from sqlalchemy.sql.functions import sum
 from sqlalchemy.orm import Session
 
-
-
-def get_movie(db: Session, movie_id: int):
-    return db.query(models.Movie).filter(models.Movie.id == movie_id).first()
-
-
-# def get_production_company_budget_by_year(db: Session, production_id: str, year: str):
-#     return db.query(models.ProductionCompany).filter(models.ProductionCompany.id == production_id)
 
 def get_production_company_budget_by_year(db: Session, production_id: str, year: str):
     budget = 0.0
@@ -24,3 +24,14 @@ def get_production_company_budget_by_year(db: Session, production_id: str, year:
     return {"year": year, "budget": budget, "company": company}
 
 
+def get_most_popular_genre_by_year(db: Session, year: str):
+
+    movie_genre = db.query(Genre.name, sum(Movie.revenue)., sum(Rating.rating), MovieGenreLookup).\
+        filter(Rating.movie_id == Movie.id).\
+        filter(MovieGenreLookup.movie_id == Movie.id).\
+        filter(Genre.id == MovieGenreLookup.genre_id). \
+        filter(Movie.release_date.like(f"%{year}-%")).\
+        group_by(Genre.name).\
+        order_by(sum(Movie.revenue, sum(Rating.rating), desc(Movie.revenue, Rating.rating))).first()
+
+    return movie_genre
